b74f902421e29be51aa6647a46a399acb5dcfb5b
Shiraswa <3489840593@qq.com>
2022-09-03 15:36:20 +0800

    修正改用String类型保存指令，同时修复了一些问题

--- src/realization.rs
+++ src/realization.rs
@@ -5,6 +5,7 @@
 最后维护时间:2022/8/26
  */
 
+use anyhow::Result;
 use tracing::{error, info, warn};
 
 //RISC-V有六种基本指令格式：
@@ -25,13 +26,14 @@ enum Type {
     U_AUIPC, //立即数地址添加到寄存器中
     J,
     Error,
-    WSSB, //我是傻逼
 }
 
 //定义RISC-V的数据结构
 struct RV {
-    registers: [u128; 128],    //定义寄存器
-    position_in_memory: usize, //定义记录内存位置
+    registers: [u128; 128],
+    //定义寄存器
+    position_in_memory: String,
+    //定义记录内存位置
     memory: [u128; 0x1000],
 }
 
@@ -39,7 +41,7 @@ impl Default for RV {
     fn default() -> Self {
         Self {
             registers: [0; 128],
-            position_in_memory: 0,
+            position_in_memory: String::new(),
             memory: [0; 0x1000],
         }
     }
@@ -47,14 +49,14 @@ impl Default for RV {
 
 impl RV {
     //运行
-    pub fn auto_run(&mut self, value: usize) {
+    pub fn auto_run(&mut self, value: String) {
         loop {
-            self.read_position_in_memory(value);
+            self.read_position_in_memory(&value);
             let opcode = self.read_opcode();
             let riscv_type = Self::type_opcode(opcode);
 
             match riscv_type {
-                Type::R => self.run_R(),
+                Type::R => Self::run_R(),
                 Type::I => todo!("我是傻逼"),
                 Type::I_LI => todo!("我是傻逼"),
                 Type::S => todo!("我是傻逼"),
@@ -68,50 +70,38 @@ impl RV {
         }
     }
     //提取出opcode
-    fn read_opcode(&self) -> usize {
-        let p = self.position_in_memory;
-        let result = ((p & 0x10F447) as usize);
+    fn read_opcode(&self) -> &str {
+        let p = &self.position_in_memory;
+        let result = &p[25..31];
         result
     }
     //读取指令
-    fn read_position_in_memory(&mut self, p: usize) {
-        self.position_in_memory = p;
+    fn read_position_in_memory(&mut self, p: &String) {
+        self.position_in_memory = String::from(p);
     }
     //重设
     pub fn reset(&mut self) {
         self.registers = [0 as u128; 128];
-        self.position_in_memory = 0 as usize;
+        self.position_in_memory = String::new();
         self.memory = [0 as u128; 0x1000];
     }
-
-    //分离R指令
-    #[warn(overflowing_literals)]
-    fn run_R(&self) {
-        //分离信息
-        let p = self.position_in_memory;
-
-        let rd = ((p & 0x102B3511D80) as usize);
-        let funct3 = ((p & 0x64F43391F000) as usize);
-        let rs1 = ((p & 0x9A3233A1A35D8000) as usize);
-        let rs2 = ((p & 0x178756E190B11BBC0000) as usize);
-        let funct7 = ((p & 0xE06317A43A087635BA7000000) as usize);
-
-        let run = R::new(rd, funct3, funct7, rs1, rs2);
-        run.auto_run();
-    }
     //读取指令，分类
-    fn type_opcode(code: usize) -> Type {
+    fn type_opcode(code: &str) -> Type {
         match code {
-            0x1ADBB => Type::R,
-            0x271B => Type::I,
-            0xB => Type::I_LI,
-            0x186AB => Type::S,
-            0x10C8EB => Type::B,
-            0x1AE1F => Type::U_LUI,
-            0x277F => Type::U_AUIPC,
-            0x10CD37 => Type::J,
+            "0110011" => Type::R,
+            "0010011" => Type::I,
+            "0000011" => Type::I_LI,
+            "0100011" => Type::S,
+            "1100011" => Type::B,
+            "0110111" => Type::U_LUI,
+            "0010111" => Type::U_AUIPC,
+            "1101111" => Type::J,
             _ => Type::Error,
         }
+    }
+    fn run_R() {
+        let run = R::default();
+        run.auto_run();
     }
 }
 
@@ -124,6 +114,7 @@ impl RV {
 // 操作字符add sub sll slt sltu xor srl sra or and
 // rs1 -> 第一个源操作数寄存器    占了5bit，在指令格式的15-19bit位上。
 // rs2 -> 第二个源操作数寄存器    占了5bit，在指令格式的25-31bit位上。
+#[derive(Default)]
 struct R {
     rd: usize,
     funct3: usize,
@@ -133,16 +124,6 @@ struct R {
 }
 
 impl R {
-    pub fn new(rd: usize, funct3: usize, funct7: usize, rs1: usize, rs2: usize) -> Self {
-        Self {
-            rd,
-            funct3,
-            funct7,
-            rs1,
-            rs2,
-        }
-    }
-
     pub fn auto_run(&self) {
         todo!("我是傻逼");
     }
